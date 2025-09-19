828591d0b01d1d0c69b6fadae925f47dc9520a40
GiaBruh <baohngce171023@fpt.edu.vn>
2025-09-19 14:45:23 +0700

    feat(front-end): add new components and pages

---
+++ web/app/[locale]/shipping/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Shipping</div>;
+};
+
+export default page;
---
+++ web/components/layout/FooterMenu.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const FooterMenuItem = () => {
+  return <></>;
+};
+
+const FooterMenu = () => {
+  return <></>;
+};
+
+export default FooterMenu();

8c718d6db14e66963a10ed4b17517466742e1eba
GiaBruh <baohngce171023@fpt.edu.vn>
2025-09-19 14:47:51 +0700

    feat(front-end): implement internalization

---
+++ web/i18n/routing.ts
@@ -0,0 +1,9 @@
+import { defineRouting } from "next-intl/routing";
+
+export const routing = defineRouting({
+  // comment here
+  locales: ["en", "vn"],
+
+  // comment here
+  defaultLocale: "en",
+});
